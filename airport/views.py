e402ea75be1069a6fcd28eda544549a2e91a4670
aLEKS-e3 <chebukin404@gmail.com>
2024-03-23 15:21:07 +0200

    Create views and serializers to handle city and country endpoints

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import Country, City
+from airport.permissions import IsAdminOrIfAuthenticatedReadOnly
+from airport.serializers import CountrySerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.select_related("country")
+    serializer_class = ""
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+
