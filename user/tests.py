d1901a0dbe5bd2a65ed8c8626dbf71b1ac30d3b9
aLEKS-e3 <chebukin404@gmail.com>
2024-03-27 11:22:13 +0200

    Cover custom user logic with tests

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class UserApiTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="boss@business.com",
+            password="JPM0rgan_"
+        )
+
+    def test_create_user_with_email(self):
+        payload = {
+            "email": "messi@leo.com",
+            "password": "F00tba11_"
+        }
+        response = self.client.post(reverse("user:create"), payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_user_with_short_password(self):
+        payload = {
+            "email": "messi@leo.com",
+            "password": "F00"
+        }
+        response = self.client.post(reverse("user:create"), payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_user(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(
+            reverse("user:manage"),
+            {
+                "email": "bigboss@business.com",
+                "password": "JPM0r9an_"
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- user/views.py
+++ user/views.py
@@ -1,25 +1,22 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    serializer_class = AuthTokenSerializer
-
-
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return get_user_model().objects.filter(
+            id=self.request.user.id
+        )
 
     def get_object(self):
         return self.request.user
